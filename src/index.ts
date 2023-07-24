d0893cbb85e1a49d03e901389d51999e99e46f81
Alex Ruheni <33921841+ruheni@users.noreply.github.com>
2023-07-24 17:06:54 +0000

    format index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,26 @@
 import { PrismaClient } from "@prisma/client/edge";
 
 export interface Env {
-	DIRECT_DATABASE_URL: string;
+  DIRECT_DATABASE_URL: string;
 }
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		const prisma = new PrismaClient({
-			datasources: {
-				db: {
-					url: env.DIRECT_DATABASE_URL
-				}
-			}
-		})
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    const prisma = new PrismaClient({
+      datasources: {
+        db: {
+          url: env.DIRECT_DATABASE_URL
+        }
+      }
+    })
 
-		const posts = await prisma.post.findMany()
-		const body = JSON.stringify(posts, null, 2)
+    const posts = await prisma.post.findMany()
+    const body = JSON.stringify(posts, null, 2)
 
-		return new Response(body, {
-			headers: {
-				"content-type": "application/json;charset=UTF-8"
-			}
-		});
-	},
+    return new Response(body, {
+      headers: {
+        "content-type": "application/json;charset=UTF-8"
+      }
+    });
+  },
 };
